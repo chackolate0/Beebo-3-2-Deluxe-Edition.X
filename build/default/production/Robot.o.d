ebd8237fe9102281270a98f4d4c4a328c0a4546b
chackolate0 <36235186+chackolate0@users.noreply.github.com>
2018-12-06 17:25:24 -0500

    first commit

---
+++ build/default/production/Robot.o.d
@@ -0,0 +1,15 @@
+build/default/production/Robot.o:  \
+Robot.c  \
+config.h  \
+adc.h  \
+btn.h  \
+lcd.h  \
+ led.h  \
+mic.h  \
+pmods.h  \
+rgbled.h  \
+srv.h  \
+ssd.h  \
+tone.h  \
+ultr.h  \
+utils.h 
---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Beebo_3_2_Deluxe_Edition.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Beebo_3_2_Deluxe_Edition.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=beebo32deluxeedition.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/beebo32deluxeedition.x.tar
